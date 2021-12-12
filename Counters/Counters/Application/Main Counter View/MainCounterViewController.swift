9867291b0a14e38a7c71c73f21aa50b746aa70fc
abnerabbey <abner.abbey@hotmail.com>
2021-12-12 16:15:22 -0600

    added view templates and extensions for the view descriptor

--- Counters/Counters/Application/Main Counter View/MainCounterViewController.swift
+++ Counters/Counters/Application/Main Counter View/MainCounterViewController.swift
@@ -10,7 +10,20 @@ import UIKit
 class MainCounterViewController: UIViewController {
     required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
     
+    private lazy var searchController: UISearchController = {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.hidesNavigationBarDuringPresentation = true
+        searchController.obscuresBackgroundDuringPresentation = false
+        return searchController
+    }()
+    
+    private lazy var tableView: UITableView = create {
+        $0.backgroundColor = UIColor(named: "Background")
+        $0.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
     let viewModel: MainCounterViewModel
+    let mainView = MainResponseView()
     
     init(factory: MainCounterViewFactory) {
         self.viewModel = factory.makeMainCounterViewModel()
@@ -28,6 +41,12 @@ class MainCounterViewController: UIViewController {
         title = viewModel.uiConfig.title
         view.backgroundColor = viewModel.uiConfig.background
         navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.searchController = searchController
+        
+        view.addSubview(tableView)
+        tableView.anchor(top: view.topAnchor, leading: view.leadingAnchor, trailing: view.trailingAnchor, bottom: view.bottomAnchor)
+        view.addSubview(mainView)
+        mainView.anchor(top: view.topAnchor, leading: view.leadingAnchor, trailing: view.trailingAnchor, bottom: view.bottomAnchor)
     }
     
 }
---
+++ Counters/Counters/Application/Main Counter View/MainResponseView.swift
@@ -0,0 +1,41 @@
+//
+//  MainResponseView.swift
+//  Counters
+//
+//  Created by Abner Abbey on 12/12/21.
+//
+
+import UIKit
+
+class MainResponseView: UIView {
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    let titleLabel: UILabel = .withConfig(.init(font: UIFont.systemFont(ofSize: 22, weight: .bold), textColor: UIColor(named: "PrimaryText")!))
+    let descriptionLabel: UILabel = .withConfig(.init(font: UIFont.systemFont(ofSize: 17, weight: .regular), textColor: UIColor(named: "SecondaryText")!))
+    let actionButton: Button = Button()
+    
+    override init(frame: CGRect = .zero) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    private func setup() {
+        addSubview(titleLabel)
+        addSubview(descriptionLabel)
+        addSubview(actionButton)
+        
+        titleLabel.centerAnchors(centerX: centerXAnchor, centerY: centerYAnchor)
+        descriptionLabel.anchor(top: titleLabel.bottomAnchor, leading: titleLabel.leadingAnchor, trailing: titleLabel.trailingAnchor, bottom: nil, padding: UIEdgeInsets(top: 8, left: 0, bottom: 0, right: 0))
+        actionButton.anchor(top: descriptionLabel.bottomAnchor, leading: nil, trailing: nil, bottom: nil, padding: UIEdgeInsets(top: 32, left: 0, bottom: 0, right: 0), size: CGSize(width: 200, height: 44))
+        actionButton.centerAnchors(centerX: centerXAnchor, centerY: nil)
+        
+        titleLabel.textAlignment = .center
+        descriptionLabel.textAlignment = .center
+        titleLabel.text = "Title! :)"
+        descriptionLabel.text = "I am a description :)"
+        actionButton.setTitle("I am a button", for: .normal)
+        
+    }
+    
+
+}
